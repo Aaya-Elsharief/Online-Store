6b762f13c2170c6789e321d56c9db55882fa4420
Aaya Elsharief <aaya.elsh@gmail.com>
2021-01-13 15:39:51 +0200

    final commit

--- site/resources/views/layouts/app.blade.php
+++ site/resources/views/layouts/app.blade.php
@@ -6,12 +6,13 @@
 <head>
     <meta charset="UTF-8" />
     <link rel="icon" href="https://img.icons8.com/plasticine/100/000000/shopping-cart.png">
+    @yield('content_head')
     <link rel="stylesheet" href="/css/Home.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-     <link rel="stylesheet" href="/css/buyDiv.css" />
 
-    @yield('content_head')
+
+
+
 
 </head>
 
@@ -43,7 +44,11 @@
 
 
     <div class="navbar">
-        <a href="/categories/view/1" class="active">★ Gifts ★</a>
+
+
+        <div class="dropdown">
+            <a href="/categories/view/1" class="active dropbtn">★ Gifts ★</a>
+        </div>
 
         <div class="dropdown">
             <a href="/categories/view/2" class="active  dropbtn ">Clothes</a>
@@ -185,24 +190,11 @@
       <!--  footor  list end -->
 
 </footer>
-<script>
-    var myModal = document.getElementById('myModal')
-    var myInput = document.getElementById('myInput')
-
-    myModal.addEventListener('shown.bs.modal', function () {
-        myInput.focus()
-    })
 
 
-    document.getElementById("btn").addEventListener("click", color);
 
-    function BuyFunction(){
 
-        document.getElementById("buyDiv").style.display='block';
 
-    }
-
-</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 </body>
--- site/resources/views/products/view.blade.php
+++ site/resources/views/products/view.blade.php
@@ -2,6 +2,8 @@
 @section('content_head')
     <title> view product  </title>
     <link rel="stylesheet" href="/css/product.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/buyDiv.css" />
     
 @endsection
 
